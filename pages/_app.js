32dc74679c4eec720d43dbc5fddd9b2db5908995
atroopa <atroopa@gmail.com>
2023-05-25 16:57:05 +0330

    import Navbar and footer in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 
 // INTERNAL IMPORTS
 import {TrackingProvider} from '../context/Tracking';
+import { Navbar, Footer } from '@/components';
 
 export default function App({ Component, pageProps }) {
   return (
