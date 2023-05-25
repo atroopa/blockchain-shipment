7b045ae29ccabb739ab7829add1b993ee4711811
atroopa <atroopa@gmail.com>
2023-05-25 16:55:56 +0330

    get index.js in coponents file structured

---
+++ components/index.js
@@ -0,0 +1,27 @@
+import Footer           from "./Footer";
+import Table            from "./Table";
+import Navbar           from "./Navbar";
+import Form             from "./Form";
+import Services         from "./Services";
+import Profile          from "./Profile";
+import GetShipment      from "./GetShipment";
+import CompeletShipment from "./CompeletShipment";
+import StartShipment    from "./StartShipment";
+
+//SVG IMPORTS
+
+
+export {
+    Footer          ,
+    Table           ,
+    Navbar          ,
+    Form            ,
+    Services        ,
+    Profile         ,
+    GetShipment     ,
+    CompeletShipment,
+    StartShipment   ,
+};
+
+
+
