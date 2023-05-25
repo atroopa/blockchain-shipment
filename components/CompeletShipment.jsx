e702c4f325efa76120f9f8984149844cd169a425
atroopa <atroopa@gmail.com>
2023-05-25 16:43:43 +0330

    make all components files structure

---
+++ components/CompeletShipment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompeletShipment = () => {
+  return (
+    <div>CompeletShipment</div>
+  )
+}
+
+export default CompeletShipment
---
+++ components/getShipment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const getShipment = () => {
+  return (
+    <div>getShipment</div>
+  )
+}
+
+export default getShipment
