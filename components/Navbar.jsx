141ae51d66c1d46cfa0964e61df7b314cdd76b10
atroopa <atroopa@gmail.com>
2023-05-26 12:24:48 +0330

    make navigation array-

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,23 @@ import {useEffect, useState, useContext} from 'react';
 import {ThemeContext} from "@/context/Tracking";
 import { Nav1, Nav2, Nav3 } from '@/components';
 
+
 const Navbar = () => {
 
   const [state, setState] = useState(false);
+  const {currentUser , connectWallet} = useContext(ThemeContext);
 
+  const navigation = [
+    {title: "Home"      , path: "#"},
+    {title: "Services"  , path: "#"},
+    {title: "Contact Us", path: "#"},
+    {title: "Erc20"     , path: "#"}
+  
+  ]
   return (
-    <div>Navbar</div>
+    <div>
+      Navbar
+    </div>
   );
 };
 
