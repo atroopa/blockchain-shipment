c6b99bee2e65d51ac205b51d80bdcbca9e2c8d4b
atroopa <atroopa@gmail.com>
2023-05-26 17:35:00 +0330

    navbar finish but not tested

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,8 @@
 import {useEffect, useState, useContext} from 'react';
 import {ThemeContext} from "@/context/Tracking";
 import { Nav1, Nav2, Nav3 } from '@/components';
-
+import Image from 'next/image';
+import Shiping from '../shiping.png';
 
 const Navbar = () => {
 
@@ -9,16 +10,76 @@ const Navbar = () => {
   const {currentUser , connectWallet} = useContext(ThemeContext);
 
   const navigation = [
-    {title: "Home"      , path: "#"},
-    {title: "Services"  , path: "#"},
-    {title: "Contact Us", path: "#"},
-    {title: "Erc20"     , path: "#"}
-  
-  ]
+    {title: "بلاکچین"      , path: "#"},
+    {title: "ارتباط با ما"  , path: "#"},
+    {title: "سرویس ها", path: "#"},
+    {title: "خانه"     , path: "#"}
+  ];
+//Home
+//Services
+// Contactus
+// Erc20
+
+  useEffect(() => {
+    document.onclick = (e) => {
+      const target = e.target;
+      if(!target.closest(".menu-btn")) setState(false);
+    };
+  }, []);
+
+
   return (
-    <div>
-      Navbar
-    </div>
+    <nav
+      className={` bg-white pb-5 md:text-sm ${
+        state
+          ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-non md:border-non md:mx-2 md:mt-0"
+          : ""
+      }`}>
+
+      <div className='gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8 '>
+        <div className='flex items-center justify-between py-5 md:block'>
+
+          <a href='javascript:void(0)' >
+            <Image src={Shiping} width={120} height={50} />
+          </a>
+
+          <div className='md:hide'>
+            <button className='menu-btn text-gray-500 hover:text-gray-800'
+              onclick={() => {setState(!state)}}      
+            >
+              {state ? Nav1 : Nav2}
+            </button>
+          </div>
+        </div>
+        <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
+          state ? "block" : "hidden"
+        } `}>
+          <ul className='font-bold text-right justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0'>
+            {navigation.map((item, idx) => {
+              return (
+                <li key={idx} className='text-gray-700 hover:text-gray-900'>
+                  <a href={item.path} className='block'>
+                    {item.title}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+          <div className='flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0'>
+            {
+              currentUser ? (
+                <p className='flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex'>{currentUser.slice(0,25)}</p>
+              ) : (
+                <button onclick={() => connectWallet()} className='flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex'>Connect Wallet <Nav3/> </button>
+              )
+            }
+          </div>
+
+        </div>
+
+      </div>
+
+    </nav>
   );
 };
 
