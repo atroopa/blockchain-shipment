b8dbc774907b0f302189d6c1ccf7c8a1a1b98e2c
atroopa <atroopa@gmail.com>
2023-05-30 21:42:49 +0330

    install merge.loadash

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,6 +28,7 @@ const Navbar = () => {
     };
   }, []);
 
+  const connectButtonColor = 'gray';
 
   return (
     <nav
@@ -71,7 +72,7 @@ const Navbar = () => {
               currentUser ? (
                 <p className='flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex'>{currentUser.slice(0,25)}</p>
               ) : (
-                <ConnectButton onClick={handleConnect} /> 
+                <ConnectButton style={{ backgroundColor: connectButtonColor }} onClick={handleConnect} /> 
               )
             }
           </div>
