2bca49df31f189e0cf4b0e71e49b66533a847530
atroopa <atroopa@gmail.com>
2023-05-31 12:28:13 +0330

    footerNav coded in footer components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import { Fot1, Fot2 } from "@/components/index";
 
 const Footer = () => {
+
+  const footerNav = [
+    {
+      href: "javascript:void()",
+      name: "terms",
+    },
+    {
+      href: "javascript:void()",
+      name: "License"
+    },
+    {
+      href: "javascript:void()",
+      name:"Privacy",
+    },
+    {
+      href: "javascript:void()",
+      name: "About Us"
+    }
+  ];
+
+
+  
   return (
     <div>Footer</div>
   );
