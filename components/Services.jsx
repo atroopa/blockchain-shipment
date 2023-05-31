6f9dc54c570ae3a19dafe398ad40f66450e1fa11
atroopa <atroopa@gmail.com>
2023-05-31 16:44:57 +0330

    create cards for services components

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+  const services = [
+    {
+      title: 'عنوان کارت ۱',
+      description: 'توضیحات کارت ۱',
+    },
+    {
+      title: 'عنوان کارت ۲',
+      description: 'توضیحات کارت ۲',
+    },
+    {
+      title: 'عنوان کارت ۳',
+      description: 'توضیحات کارت ۳',
+    },
+    {
+      title: 'عنوان کارت ۴',
+      description: 'توضیحات کارت ۴',
+    },
+    {
+      title: 'عنوان کارت ۵',
+      description: 'توضیحات کارت ۵',
+    },
+    {
+      title: 'عنوان کارت ۶',
+      description: 'توضیحات کارت ۶',
+    },
+  ];
 
-export default Services
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {services.map((service, index) => (
+        <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-4">{service.title}</h2>
+          <p className="text-gray-600">{service.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Services;
