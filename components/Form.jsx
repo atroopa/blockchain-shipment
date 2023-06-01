2d96f8994a9f5c31c557d9472e7b5729b2fb1cc6
atroopa <atroopa@gmail.com>
2023-06-01 13:02:51 +0330

    try to coding form

--- components/Form.jsx
+++ components/Form.jsx
@@ -10,12 +10,81 @@ const Form = ({setCreateShipmentModel, createShipmentModel, createShipment}) => 
     price:      ""
   });
   
-  
-  return (
-    <div>
-      
+  const createItem = async() => {
+    try {
+      await createShipment(shipment);
+    } catch (error) {
+      console.log("from FORM Component: wrong creating item ");
+    }
+  }
+
+  return createShipmentModel ? (
+    <div className="fixed inset-0 z-10 overflow-y-auto ">
+      <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={setCreateShipmentModel(false)}>
+
+      </div>
+      <div className="flex items-center min-h-screen px-4 py-8">
+        <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+          <div className="flex justify-end">
+            <button className="p-2 text-gray-400 rounded-md hover:bg-gray-100" onClick={() => setCreateShipmentModel(false)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5 mx-auto"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                >
+                  path
+              </svg>
+            </button>
+          </div>
+          <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+            <h4 className="text-lg font-medium text-gray-800">
+              Track Product, Create Shipment
+            </h4>
+            <p className="text-[15px] text-gray-600">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem eos iure ab cum dolores repellendus qui non, amet dolorum voluptatem?
+            </p>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <div className="relative mt-3">
+                <inpu
+                  type="text"
+                  placeholder = "receiver"
+                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  onChange = {(e) => 
+                                setShipment({
+                                  ...shipment,
+                                  receiver: e.target.value,
+                                })
+                              }
+                    />
+              </div>
+              <div className="relative mt-3">
+                  <input 
+                    type="date"
+                    placeholder="pickupTime"
+                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                    onChange={(e) => 
+                              setShipment({
+                                ...shipment,
+                                pickupTime: e.target.value
+                              })  
+                            }
+                  />
+              </div>
+              <div className="relative mt-3">
+                <input 
+                  type="text"
+                  placeholder="distance"
+                  className="w-full" 
+                
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  ) : ""
 }
 
 export default Form
