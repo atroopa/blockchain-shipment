b27c61b4deaad68c722c1d33b4e04ec269fb63d9
atroopa <atroopa@gmail.com>
2023-06-01 17:47:13 +0330

    change form components to persian

--- components/Form.jsx
+++ components/Form.jsx
@@ -29,29 +29,34 @@ const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) =
               className="p-2 text-gray-400 rounded-md hover:bg-gray-100"
               onClick={() => setCreateShipmentModel(false)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 mx-auto"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  onClick={() => setCreateShipmentModel(false)}
+                >
+                  <path d="M12 6 L6 12 M6 6 L12 12" />
+                </svg>
             </button>
           </div>
-          <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+          <div className="max-w-sm mx-auto py-3 space-y-3 text-right">
             <h4 className="text-lg font-medium text-gray-800">
-              Track Product, Create Shipment
+            پیگیری محصول، ایجاد حمل و نقل
             </h4>
             <p className="text-[15px] text-gray-600">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem eos iure ab cum dolores repellendus qui non, amet dolorum voluptatem?
+             در اینجا یک حمل و نقل ایجاد می شود . تمامی حمل و نقل ها تا قبل از بارگیری قابل ویرایش می باشند لطفا در درستی موارد دقت بفرمایید
             </p>
             <form onSubmit={(e) => e.preventDefault()}>
               <div className="relative mt-3">
                 <input
                   type="text"
-                  placeholder="receiver"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  placeholder="آدرس دریافت کننده"
+                  className="text-right w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   onChange={(e) =>
                     setShipment({
                       ...shipment,
@@ -63,8 +68,8 @@ const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) =
               <div className="relative mt-3">
                 <input
                   type="date"
-                  placeholder="pickupTime"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  placeholder="زمان بارگیری به میلادی"
+                  className=" w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   onChange={(e) =>
                     setShipment({
                       ...shipment,
@@ -76,8 +81,8 @@ const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) =
               <div className="relative mt-3">
                 <input
                   type="text"
-                  placeholder="distance"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  placeholder="مسافت"
+                  className="text-right w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   onChange={(e) =>
                     setShipment({
                       ...shipment,
@@ -89,8 +94,8 @@ const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) =
               <div className="relative mt-3">
                 <input
                   type="text"
-                  placeholder="price"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  placeholder="قیمت"
+                  className="text-right w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   onChange={(e) =>
                     setShipment({
                       ...shipment,
@@ -103,7 +108,7 @@ const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) =
                 onClick={() => createItem()}
                 className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2"
               >
-                Create Shipment
+                ایجاد حمل و نقل
               </button>
             </form>
           </div>
