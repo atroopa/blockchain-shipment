00cd9b2506fdb37f2ed40b26436bb23f5569efa9
atroopa <atroopa@gmail.com>
2023-06-01 13:07:28 +0330

    inputs for form

--- components/Form.jsx
+++ components/Form.jsx
@@ -75,8 +75,13 @@ const Form = ({setCreateShipmentModel, createShipmentModel, createShipment}) => 
                 <input 
                   type="text"
                   placeholder="distance"
-                  className="w-full" 
-                
+                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg" 
+                  onChange={(e) => 
+                            setShipment({
+                              ...shipment,
+                              distance: e.target.value
+                            })
+                          }
                 />
               </div>
             </form>
