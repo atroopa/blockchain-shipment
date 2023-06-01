19add5001791dc0b008a3087ff3a5e672020e915
atroopa <atroopa@gmail.com>
2023-06-01 13:26:01 +0330

    form bug fix and work true

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,39 +1,41 @@
 import { useState } from "react";
 
-
-const Form = ({setCreateShipmentModel, createShipmentModel, createShipment}) => {
-  
+const Form = ({ setCreateShipmentModel, createShipmentModel, createShipment }) => {
   const [shipment, setShipment] = useState({
-    receiver:   "",
+    receiver: "",
     pickupTime: "",
-    distance:   "",
-    price:      ""
+    distance: "",
+    price: ""
   });
-  
-  const createItem = async() => {
+
+  const createItem = async () => {
     try {
       await createShipment(shipment);
     } catch (error) {
       console.log("from FORM Component: wrong creating item ");
     }
-  }
+  };
 
   return createShipmentModel ? (
     <div className="fixed inset-0 z-10 overflow-y-auto ">
-      <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={setCreateShipmentModel(false)}>
-
-      </div>
+      <div
+        className="fixed inset-0 w-full h-full bg-black opacity-40"
+        onClick={() => setCreateShipmentModel(false)}
+      ></div>
       <div className="flex items-center min-h-screen px-4 py-8">
         <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
           <div className="flex justify-end">
-            <button className="p-2 text-gray-400 rounded-md hover:bg-gray-100" onClick={() => setCreateShipmentModel(false)}>
+            <button
+              className="p-2 text-gray-400 rounded-md hover:bg-gray-100"
+              onClick={() => setCreateShipmentModel(false)}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-5 h-5 mx-auto"
                 viewBox="0 0 20 20"
                 fill="currentColor"
-                >
-                  path
+              >
+                <path />
               </svg>
             </button>
           </div>
@@ -46,60 +48,60 @@ const Form = ({setCreateShipmentModel, createShipmentModel, createShipment}) => 
             </p>
             <form onSubmit={(e) => e.preventDefault()}>
               <div className="relative mt-3">
-                <inpu
+                <input
                   type="text"
-                  placeholder = "receiver"
+                  placeholder="receiver"
                   className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                  onChange = {(e) => 
-                                setShipment({
-                                  ...shipment,
-                                  receiver: e.target.value,
-                                })
-                              }
-                    />
+                  onChange={(e) =>
+                    setShipment({
+                      ...shipment,
+                      receiver: e.target.value
+                    })
+                  }
+                />
               </div>
               <div className="relative mt-3">
-                  <input 
-                    type="date"
-                    placeholder="pickupTime"
-                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                    onChange={(e) => 
-                              setShipment({
-                                ...shipment,
-                                pickupTime: e.target.value
-                              })  
-                            }
-                  />
+                <input
+                  type="date"
+                  placeholder="pickupTime"
+                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  onChange={(e) =>
+                    setShipment({
+                      ...shipment,
+                      pickupTime: e.target.value
+                    })
+                  }
+                />
               </div>
               <div className="relative mt-3">
-                <input 
+                <input
                   type="text"
                   placeholder="distance"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg" 
-                  onChange={(e) => 
-                            setShipment({
-                              ...shipment,
-                              distance: e.target.value
-                            })
-                          }
+                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  onChange={(e) =>
+                    setShipment({
+                      ...shipment,
+                      distance: e.target.value
+                    })
+                  }
                 />
               </div>
               <div className="relative mt-3">
-                <input 
+                <input
                   type="text"
                   placeholder="price"
-                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg" 
-                  onChange={(e) => 
-                            setShipment({
-                              ...shipment,
-                              price: e.target.value
-                            })
-                          }
+                  className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  onChange={(e) =>
+                    setShipment({
+                      ...shipment,
+                      price: e.target.value
+                    })
+                  }
                 />
               </div>
-              <button 
-              onClick={() => createItem()}              
-              className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2"
+              <button
+                onClick={() => createItem()}
+                className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2"
               >
                 Create Shipment
               </button>
@@ -111,6 +113,6 @@ const Form = ({setCreateShipmentModel, createShipmentModel, createShipment}) => 
   ) : (
     ""
   );
-}
+};
 
-export default Form
+export default Form;
