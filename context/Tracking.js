560ef432e2d4b47faf8b4622774ebfb9353e6133
atroopa <atroopa@gmail.com>
2023-06-06 20:51:20 +0330

    fixed Bug

--- context/Tracking.js
+++ context/Tracking.js
@@ -145,16 +145,16 @@ export const TrackingProvider = ({ children }) => {
                 const ganache        = Blockchain.ganacheAddress;
                 const provider       = new ethers.providers.JsonRpcProvider(Blockchain.API_URL);
                 const contract       = fetchContract(provider);
-                const shipment       = contract.getShipment(ganache, index * 1)
-                .then(shipment => {
-                    console.log("Shipment:", shipment);
-                    // دسترسی به نتیجه‌ی کامل promise و انجام عملیات مورد نیاز
-                  })
-                  .catch(error => {
-                    console.error("Error:", error);
-                    // بررسی و رفع خطاهای احتمالی
-                  });
-                const singleShiplent = {
+                const shipment       = await contract.getShipment(ganache, index * 1)
+                // .then(shipment => {
+                //     console.log("Shipment:", shipment);
+                //     // دسترسی به نتیجه‌ی کامل promise و انجام عملیات مورد نیاز
+                //   })
+                //   .catch(error => {
+                //     console.error("Error:", error);
+                //     // بررسی و رفع خطاهای احتمالی
+                //   });
+                const singleShipmnet = {
                     sender      : shipment[0],
                     receiver    : shipment[1],
                     pickupTime  : shipment[2].toNumber(),
@@ -164,8 +164,8 @@ export const TrackingProvider = ({ children }) => {
                     status      : shipment[6],
                     isPaid      : shipment[7]
                 }
-    
-                return singleShiplent;
+                console.log(singleShipmnet);
+                return singleShipmnet;
     
             } catch (error) {
                 console.error("Sorry no Shipment");
