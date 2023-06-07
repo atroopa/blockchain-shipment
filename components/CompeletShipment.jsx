58084fba6ae605cdb822de5318e73ba9868a15a3
atroopa <atroopa@gmail.com>
2023-06-07 19:05:08 +0330

    compeleteShipment finish

--- components/CompeletShipment.jsx
+++ components/CompeletShipment.jsx
@@ -45,9 +45,20 @@ const CompeletShipment = ({compeleteModel , setCompeleteModel, compeletShipment}
                       type='number'
                       placeholder='ID'
                       className='w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg '
-                      
+                      onChange={(e) => 
+                                  setCompleteShip({
+                                    ...completeShip,
+                                    index: e.target.value
+                                  })
+                      } 
                     />
                 </div>
+                <button 
+                  onClick={() => changeStatus()}
+                  className='block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white  bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2'
+                >
+                  Change Status
+                </button>
               </form>
             </div>
           </div>
